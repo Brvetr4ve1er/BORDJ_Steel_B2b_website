c9b09b6de245fd0f8d9ee725b3024f8b2e512d3a
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-08-04 12:11:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,19 +6,12 @@ import { companyData } from '@/config/company-data';
 import { Logo } from './logo';
 import { cn } from '@/lib/utils';
 
-const TikTokIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6">
-        <path d="M21 7.25a6.25 6.25 0 1 0-8.8-6.1V15a3.75 3.75 0 1 0 3.75-3.75"/>
-    </svg>
-)
-
 export function Footer() {
   const { footer, socials, navigation, pages } = companyData;
 
   const socialIcons = [
     { href: socials.facebook, icon: <Facebook />, name: 'Facebook' },
     { href: socials.instagram, icon: <Instagram />, name: 'Instagram' },
-    { href: socials.tiktok, icon: <TikTokIcon />, name: 'TikTok' },
     { href: socials.whatsapp, icon: <MessageCircle />, name: 'WhatsApp' },
     { href: socials.x, icon: <Twitter />, name: 'X' },
   ];
