c9e61170ebaf71f191869865203287dce0bdd847
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-08-21 10:57:12 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/product-image-gallery.tsx
+++ src/components/product-image-gallery.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Image from 'next/image';
@@ -10,12 +11,6 @@ export type ProductImage = {
   alt: string;
   aiHint: string;
 };
-
-const galleryImages: ProductImage[] = [
-  { src: 'https://images.unsplash.com/photo-1501664908582-764f9d0b23ea?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxzdGVlbCUyMHNoZWV0c3xlbnwwfHx8fDE3NTU1MDE5ODN8MA&ixlib=rb-4.1.0&q=80&w=1080', alt: 'Ribbed steel sheets stacked', aiHint: 'steel sheets' },
-  { src: 'https://images.unsplash.com/photo-1735044902465-fe2f3eafdf77?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxtZXRhbCUyMHJvb2Zpbmd8ZW58MHx8fHwxNzU1NTAxOTgzfDA&ixlib=rb-4.1.0&q=80&w=1080', alt: 'Close-up of metal roofing sheets', aiHint: 'metal roofing' },
-  { src: 'https://images.unsplash.com/photo-1578776349090-de61da00ff1a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxmYWN0b3J5JTIwYnVpbGRpbmd8ZW58MHx8fHwxNzU1NTAxOTgzfDA&ixlib=rb-4.1.0&q=80&w=1080', alt: 'Factory building with sandwich panel walls', aiHint: 'factory building' },
-];
 
 interface ProductImageGalleryProps {
     images: ProductImage[];
