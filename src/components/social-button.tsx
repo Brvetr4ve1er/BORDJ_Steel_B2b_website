2535d393d9cbe40e2d819fa095ea0e3f596c5141
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-10-06 10:59:21 +0000

    make it smaller  (_for element div_)

--- src/components/social-button.tsx
+++ src/components/social-button.tsx
@@ -16,16 +16,16 @@ interface SocialButtonProps {
 export function SocialButton({ href, children, className, 'aria-label': ariaLabel, fromColor = 'from-cyan-700', toColor = 'to-cyan-400' }: SocialButtonProps) {
   return (
     <Link href={href} aria-label={ariaLabel} target="_blank" className="group">
-      <div className={cn("relative w-[100px] h-[100px] transition-all duration-200 ease-in-out hover:scale-110 active:scale-75", className)}>
+      <div className={cn("relative w-[80px] h-[80px] transition-all duration-200 ease-in-out hover:scale-110 active:scale-75", className)}>
         {/* Main Button Container */}
-        <div className="w-full h-full rounded-full overflow-hidden relative grid place-content-center border-[5px] border-r-white border-l-gray-800/20 border-t-white/50 border-b-gray-800/50 transform -rotate-45 transition-all duration-500 ease-in-out">
+        <div className="w-full h-full rounded-full overflow-hidden relative grid place-content-center border-[4px] border-r-white border-l-gray-800/20 border-t-white/50 border-b-gray-800/50 transform -rotate-45 transition-all duration-500 ease-in-out">
           {/* Inner Button */}
-          <div className="relative w-[54px] h-[54px] rounded-full shadow-[-10px_5px_10px_0px_rgba(100,100,111,0.5)] transition-all duration-500 ease-in-out active:scale-125">
+          <div className="relative w-[44px] h-[44px] rounded-full shadow-[-8px_4px_8px_0px_rgba(100,100,111,0.5)] transition-all duration-500 ease-in-out active:scale-125">
             {/* Background Gradient */}
             <div className={cn("absolute inset-0 rounded-full bg-gradient-to-tr", fromColor, toColor)} />
             {/* Foreground Gradient & Icon */}
-            <div className={cn("absolute inset-[5px] rounded-full bg-gradient-to-tr grid place-content-center", fromColor, toColor)}>
-              <div className="transform rotate-45 text-white/50 group-hover:text-white group-hover:opacity-100 transition-all duration-200 ease-in group-hover:[filter:drop-shadow(0_0_10px_white)]">
+            <div className={cn("absolute inset-[4px] rounded-full bg-gradient-to-tr grid place-content-center", fromColor, toColor)}>
+              <div className="transform rotate-45 text-white/50 group-hover:text-white group-hover:opacity-100 transition-all duration-200 ease-in group-hover:[filter:drop-shadow(0_0_8px_white)]">
                 {children}
               </div>
             </div>
