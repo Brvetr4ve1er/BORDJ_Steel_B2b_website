7087f587dde9e239016b383128f15076737fc69a
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-10-20 09:42:38 +0000

    diagnose and audit if we broke anything

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import './shiny-button.css';
 import './download-button.css';
 import { Toaster } from "@/components/ui/toaster"
-import { Montserrat, Roboto } from 'next/font/google';
+import { Montserrat, Roboto, Cairo } from 'next/font/google';
 import { companyData } from '@/config/company-data';
 
 const montserrat = Montserrat({
@@ -18,6 +18,13 @@ const roboto = Roboto({
   display: 'swap',
   variable: '--font-roboto',
   weight: ['400', '500', '700'],
+});
+
+const cairo = Cairo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-cairo',
+  weight: ['700'],
 });
 
 export const metadata: Metadata = {
@@ -48,7 +55,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" className={`${montserrat.variable} ${roboto.variable}`}>
+    <html lang="fr" className={`${montserrat.variable} ${roboto.variable} ${cairo.variable}`}>
       <head>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
