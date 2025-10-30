e8bf27dcb1dca192ad72baa1f273a6b1028ab3ad
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-10-30 14:07:50 +0000

    i need you to replace the images in this section each with its own  imag

--- src/components/charpente-metallique-page.tsx
+++ src/components/charpente-metallique-page.tsx
@@ -129,12 +129,11 @@ const NewGallery = () => {
             </div>
             <div className="flex items-center gap-2 h-[400px] w-full max-w-7xl mt-10 px-4">
                 {[
-                    "https://images.unsplash.com/photo-1719368472026-dc26f70a9b76?q=80&h=800&w=800&auto=format&fit=crop",
-                    "https://images.unsplash.com/photo-1649265825072-f7dd6942baed?q=80&h=800&w=800&auto=format&fit=crop",
-                    "https://images.unsplash.com/photo-1555212697-194d092e3b8f?q=80&h=800&w=800&auto=format&fit=crop",
-                    "https://images.unsplash.com/photo-1729086046027-09979ade13fd?q=80&h=800&w=800&auto=format&fit=crop",
-                    "https://images.unsplash.com/photo-1601568494843-772eb04aca5d?q=80&h=800&w=800&auto=format&fit=crop",
-                    "https://images.unsplash.com/photo-1585687501004-615dfdfde7f1?q=80&h=800&w=800&auto=format&fit=crop",
+                    "https://i.pinimg.com/474x/e9/8f/d0/e98fd0f2be33adea96cf129e977d6dbf.jpg",
+                    "https://i.pinimg.com/736x/53/07/e6/5307e6787500b6efff734990a41772e5.jpg",
+                    "https://i.pinimg.com/736x/5c/c7/70/5cc77095f582fa54a8cd4188eda88705.jpg",
+                    "https://i.pinimg.com/736x/21/93/79/219379e4219680d5c06be55fecfcdbaf.jpg",
+                    "https://i.pinimg.com/736x/ec/93/b8/ec93b8a90b0c088c23cdf817613dd183.jpg",
                 ].map((src, idx) => (
                     <div
                         key={idx}
