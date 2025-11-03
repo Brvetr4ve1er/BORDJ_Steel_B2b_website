7fec86e3bc7ce062394523a7dc213b140b54c398
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-11-03 09:28:53 +0000

    the  image is not didsplaying

--- src/components/product-variants/couverture-product.tsx
+++ src/components/product-variants/couverture-product.tsx
@@ -257,7 +257,7 @@ export default function CouvertureProduct({ product }: { product: any }) {
                <div className="border-2 border-border bg-secondary/10 p-4">
                  <div className="bg-white border border-border p-4 flex items-center justify-center">
                    <Image 
-                    src="https://i.pinimg.com/736x/76/d6/80/76d680d6fdc50e1f996f5fe8ccc5029d.jpg" 
+                    src="https://i.ibb.co/ZzkydPSY/couverture.png" 
                     alt="Schéma technique"
                     width={600}
                     height={200}
@@ -275,3 +275,5 @@ export default function CouvertureProduct({ product }: { product: any }) {
       </div>
     );
   };
+
+    
