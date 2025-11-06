ebd016359a171a6bd56efb193df3252acb096d68
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-11-06 09:57:21 +0000

    i want it to be a 3by3 grid (_for element <TeamsSection>_)

--- src/app/about/history/page.tsx
+++ src/app/about/history/page.tsx
@@ -191,7 +191,7 @@ const TeamFeature = ({
   image: string;
 }) => {
   const totalFeatures = teams.length;
-  const numCols = 2; // lg:grid-cols-2
+  const numCols = 3; // lg:grid-cols-3
   return (
     <div
       className={cn(
@@ -236,7 +236,7 @@ function TeamsSection() {
       <AnimatedWrapper animation="fade-in">
         <h2 className="text-6xl md:text-7xl font-bold text-primary mb-12 text-center">Nos équipes spécialisées</h2>
       </AnimatedWrapper>
-       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 relative z-10 py-10 max-w-7xl mx-auto gap-4">
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 relative z-10 py-10 max-w-7xl mx-auto gap-4">
         {teams.map((team, index) => (
             <TeamFeature key={team.id} {...team} index={index} />
         ))}
