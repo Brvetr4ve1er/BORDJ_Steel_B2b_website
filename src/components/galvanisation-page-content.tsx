a4a11b88f3066f7e0bef6588f3fea5b856bfde90
br4ve tr4aveler <brvetr4veler@gmail.com>
2025-11-11 10:01:03 +0000

    make this part of the text red and bold  (   Ces diérentes couches d’al

--- src/components/galvanisation-page-content.tsx
+++ src/components/galvanisation-page-content.tsx
@@ -214,7 +214,7 @@ function UnwrappedBenefitsSection() {
               « Une protection anticorrosion qui va au-delà d’un simple dépôt de zinc »
             </p>
              <p className="text-lg text-muted-foreground max-w-4xl mx-auto mb-16 text-left">
-                La galvanisation à chaud ne consiste pas uniquement à déposer du zinc à la surface de l’acier. Le revêtement de zinc est métallurgiquement lié à l’acier de base, car il se produit une réaction métallurgique de diffusion entre le zinc et le fer. Quand on retire l’acier du bain, il s’est formé à sa surface plusieurs couches d’alliages zinc-fer sur lesquelles le zinc entraîné se solidifie. Ces différentes couches d’alliages plus dures que l’acier de base ont une teneur en zinc de plus en plus élevée au fur et à mesure que l’on se rapproche de la surface du revêtement. Ainsi, cette spécificité liée au procédé de galvanisation offre au revêtement protecteur adhérence, imperméabilité, et résistance mécanique. De plus, l’épaisseur de ce est supérieure à celle obtenue avec les autres techniques de protection
+                La galvanisation à chaud ne consiste pas uniquement à déposer du zinc à la surface de l’acier. Le revêtement de zinc est métallurgiquement lié à l’acier de base, car il se produit une réaction métallurgique de diffusion entre le zinc et le fer. Quand on retire l’acier du bain, il s’est formé à sa surface plusieurs couches d’alliages zinc-fer sur lesquelles le zinc entraîné se solidifie. <span className="text-accent font-bold">Ces différentes couches d’alliages plus dures que l’acier de base ont une teneur en zinc de plus en plus élevée au fur et à mesure que l’on se rapproche de la surface du revêtement.</span> Ainsi, cette spécificité liée au procédé de galvanisation offre au revêtement protecteur adhérence, imperméabilité, et résistance mécanique. De plus, l’épaisseur de ce est supérieure à celle obtenue avec les autres techniques de protection
             </p>
           </AnimatedWrapper>
           <div className="grid md:grid-cols-3 gap-8">
@@ -306,5 +306,7 @@ function UnwrappedCTASection() {
       </section>
     );
 }
+
+    
 
     
